05b30797f20d3fd62207f9cc87236d1cbc97d30d
Petro Malamule <133478698+Shaunpetro@users.noreply.github.com>
2024-04-19 21:16:35 +0200

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# creates a file in /tmp
-
-file { '/tmp/school':
-  content =>'I love Puppet',
+# A resource declaration
+file { '/tmp/codingschool':
+  ensure  => file,
+  path    => '/tmp/codingschool',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet'
 }
