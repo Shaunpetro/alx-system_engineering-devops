711b38d8f2841b1a92564d7618aa2f7334d773b1
shaunpetro <shaun.p.mckoy@gmail.com>
2024-06-04 21:23:57 +0200

    first tasks

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+"""prints hot posts on given Reddit subreddit"""
+import requests
+
+def top_ten(subreddit):
+    """prints titles of the 10 hottests posts on a subreddit"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User_Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
